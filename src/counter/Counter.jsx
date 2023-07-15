cab2f2e0f9f531f39b3ad413f408b798c5bc9359
steevecode <stevecode@yahoo.com>
2023-07-15 12:00:44 +0100

    Step 04 - Exploring React State with useState hook - Adding state to Counter

--- src/counter/Counter.jsx
+++ src/counter/Counter.jsx
@@ -1,15 +1,20 @@
+import { useState } from 'react'
 import './Counter.css'
 
 export default function Counter() {
+
+    // [0, f]
+    const [count, setCount] = useState(0);
     
     
-    function incrementCounterParentFunction(by) {
-       console.log('increment clicked')
+    function incrementCounterParentFunction() {
+
+       setCount(count + 1)
     }
     
     return (
         <div className="Counter">
-            <span className="count">0</span>
+            <span className="count">{count}</span>
             <div>            
                 <button className="counterButton" onClick={incrementCounterParentFunction} >+1</button></div>
             </div>
